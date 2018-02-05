43f4bffcbf53e4762553890968666708bc017661
XcXerxes <xcxerxes@aliyun.com>
2018-02-05 23:48:10 +0800

    update init

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LoginHome extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default LoginHome;
